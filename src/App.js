8c99d8506a536bd3ca508402ff7398e609ab5e9e
Munir Mahmud <munirmahmud6@gmail.com>
2020-10-14 11:10:18 +0600

    Header component added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { JumbotronContainer } from "./containers/JumbotronContainer";
 import { FooterContainer } from "./containers/FooterContainer";
 import { AccordionContainer } from "./containers/AccordionComponent";
+import { HeaderContainer } from "./containers/HeaderComponent";
 
 export default function App() {
     return (
         <>
+            <HeaderContainer />
             <JumbotronContainer />
             <AccordionContainer />
             <FooterContainer />
---
+++ src/components/header/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Background, Container, Logo, ButtonLink } from './styles/header';
+import { Link as RouterLink } from 'react-router-dom';
+
+export default function Header({bg = true, children, ...restProps}) {
+    return bg ? <Background {...restProps}>{children}</Background> : {children};
+}
+
+Header.Frame = function HeaderFrame({children, ...restProps}) {
+    return <Container {...restProps}>{children}</Container>;
+};
+
+Header.Logo = function HeaderLogo({...restProps}) {
+    return (
+            <Logo {...restProps} />
+        );
+};
+
+Header.ButtonLink = function HeaderButtonLink({children, ...restProps}) {
+    return <ButtonLink {...restProps}>{children}</ButtonLink>;
+};
