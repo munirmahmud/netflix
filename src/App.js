a885068a227eaa9751edb14b807fe3b21944c061
Munir Mahmud <munirmahmud6@gmail.com>
2020-10-14 10:57:48 +0600

    Accordion component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { JumbotronContainer } from "./containers/JumbotronContainer";
 import { FooterContainer } from "./containers/FooterContainer";
+import { AccordionContainer } from "./containers/AccordionComponent";
 
 export default function App() {
     return (
         <>
             <JumbotronContainer />
+            <AccordionContainer />
             <FooterContainer />
         </>
     )
