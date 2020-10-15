1c17439680ec92a485a794c3589c960d7567eec7
Munir Mahmud <munirmahmud6@gmail.com>
2020-10-15 19:45:00 +0600

    User redirect added for Sign in & Sign up pages

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import { Browse, Home, Signin, Signup } from "./pages";
+import { IsUserRedirect } from "./helpers/routes";
 
 export default function App() {
+    const user = {};
+
     return (
         <>
             <Router>
+                <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} exact >
+                    <Signin />
+                </IsUserRedirect>
+
+                <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP} exact >
+                    <Signup />
+                </IsUserRedirect>
+
                 <Route exact path={ROUTES.HOME}>
                     <Home />
                 </Route>
-                <Route exact path={ROUTES.SIGN_IN}>
-                    <Signin />
-                </Route>
-
-                <Route exact path={ROUTES.SIGN_UP}>
-                    <Signup />
-                </Route>
-
                 <Route exact path={ROUTES.BROWSE}>
                     <Browse />
                 </Route>
