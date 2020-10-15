d9f20a540a91053de73899783a1595d0bcc1278a
Munir Mahmud <munirmahmud6@gmail.com>
2020-10-15 09:43:40 +0600

    Header component is done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,24 @@ import { JumbotronContainer } from "../containers/JumbotronContainer";
 import { FooterContainer } from "../containers/FooterContainer";
 import { AccordionContainer } from "../containers/AccordionComponent";
 import { HeaderContainer } from "../containers/HeaderComponent";
+import { OptForm } from "../components";
+import Feature from "../components/feature";
 
 export default function Home() {
     return (
         <>
             <HeaderContainer>
-                <h1>This is Header</h1>
+                <Feature>
+                    <Feature.Title>Unlimited movies, TV shows, and more.</Feature.Title>
+                    <Feature.SubTitle>Watch anywhere. Cancel anytime.</Feature.SubTitle>
+
+                    <OptForm>
+                        <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+                        <OptForm.Break />
+                        <OptForm.Input type="text" placeholder="Email address" />
+                        <OptForm.Button type="submit">Try it now</OptForm.Button>
+                    </OptForm>
+                </Feature>
             </HeaderContainer>
 
             <JumbotronContainer />
