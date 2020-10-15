37dfb21c89b828ad52f1700592eac17a2d7c90fe
Munir Mahmud <munirmahmud6@gmail.com>
2020-10-15 16:47:08 +0600

    Sign up page & authentication is done

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { useHistory } from 'react-router-dom';
+import Form from "../components/form";
+import {HeaderContainer} from "../containers/HeaderComponent";
+import {FooterContainer} from "../containers/FooterContainer";
+import { FirebaseContext } from '../context/firebaseContext';
+import * as ROUTES from '../constants/routes';
 
 export default function Signup() {
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
+
+    const [firstName, setFirstName] = useState('');
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const isInvalid = firstName === '' || emailAddress === '' || password === '';
+
+    const handleSignUp = (e) => {
+        e.preventDefault();
+
+        firebase.auth()
+            .createUserWithEmailAndPassword(emailAddress, password)
+            .then(result => {
+                result.user.updateProfile({
+                    displayName: firstName,
+                    photoURL: Math.floor(Math.random() * 5) + 1,
+                })
+                .then(() => {
+                    history.push(ROUTES.BROWSE)
+                })
+            })
+            .catch((error) => {
+                setFirstName('');
+                setEmailAddress('');
+                setPassword('');
+                setError(error.message);
+            })
+    }
+
     return (
-        <h1>This is Sign up page</h1>
+        <>
+            <HeaderContainer>
+                <Form>
+                    <Form.Title>Sign Up</Form.Title>
+                    {error && <Form.Error>{error}</Form.Error>}
+
+                    <Form.Base onSubmit={handleSignUp} method="POST">
+                        <Form.Input type="text" placeholder="First Name" value={firstName} onChange={({ target }) => setFirstName(target.value)} />
+                        <Form.Input type="email" placeholder="Email Address" value={emailAddress} onChange={({ target }) => setEmailAddress(target.value)} />
+                        <Form.Input type="password" placeholder="Password" value={password} onChange={({ target }) => setPassword(target.value)} autocomplete="off" />
+                        <Form.Submit type="submit" disabled={isInvalid}>Sign Up</Form.Submit>
+                    </Form.Base>
+
+                    <Form.Text>Already a user? <Form.Link to={ROUTES.SIGN_IN}>Sign in now.</Form.Link></Form.Text>
+                    <Form.TextSmall>This page is protected by Google reCAPTCHS to ensure you're not a bot. Learn More</Form.TextSmall>
+                </Form>
+            </HeaderContainer>
+
+            <FooterContainer />
+        </>
     );
 }
