e9d872e1846070ecf26cf51c45028e1aae023214
Munir Mahmud <munirmahmud6@gmail.com>
2020-10-15 10:26:17 +0600

    Sign in form component with styles done

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
+import { FooterContainer } from "../containers/FooterContainer";
+import { HeaderContainer } from "../containers/HeaderComponent";
+import Form from "../components/form";
+import * as ROUTES from '../constants/routes';
 
 export default function Signin() {
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const isInvalid = emailAddress === '' || password === '';
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+
+    };
+
     return (
-        <h1>This is Sign in page</h1>
+        <>
+            <HeaderContainer>
+                <Form>
+                    <Form.Title>Sign In</Form.Title>
+                    {error && <Form.Error>{error}</Form.Error>}
+
+                    <Form.Base onSubmit={handleSubmit}>
+                        <Form.Input type="email" placeholder="Email address"  />
+                        <Form.Input type="password" placeholder="Password"  />
+                        <Form.Submit type="submit" disabled={isInvalid}>Sign In</Form.Submit>
+                    </Form.Base>
+
+                    <Form.Text>New to Netflix? <Form.Link to={ROUTES.SIGN_UP}>Sign up now.</Form.Link></Form.Text>
+                    <Form.TextSmall>This page is protected by Google reCAPTCHA to ensure you're not a bot. </Form.TextSmall>
+                </Form>
+            </HeaderContainer>
+
+            <FooterContainer />
+        </>
     );
 }
