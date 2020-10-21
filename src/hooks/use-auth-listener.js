47b2fe9b9973b38cd9705c68cd69ef9460e70f5a
Munir Mahmud <munirmahmud6@gmail.com>
2020-10-21 08:13:41 +0600

    Removed unused hook

--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -1,4 +1,4 @@
-import {useState, useEffect, createContext, useContext} from "react";
+import {useState, useEffect, useContext} from "react";
 import { FirebaseContext } from "../context/firebaseContext";
 
 export default function useAuthListener() {
